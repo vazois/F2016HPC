9d3af051bdc26effe32bf2f7d2275fc504874390
Vasileios Zois <vzois@usc.edu>
2016-09-23 18:22:33 -0700

    finished dgemm0 and dgemm1

--- A01/main.cpp
+++ A01/main.cpp
@@ -2,17 +2,54 @@
 #include "tools/Utils.h"
 #include "tools/ArgParser.h"
 
-void initMatrix(double *&A, double *&B, unsigned int N){
+void initMatrix(double *&A, double *&B, double *&C, unsigned int N){
 	Utils<double> u;
 	for(unsigned int i = 0 ; i < N;i++){
 		A[i] = u.uni(-2,5);
 		B[i] = u.uni(-2,5);
+		C[i] = 0;
 	}
 }
 
-void dgemm0(const double *A, const double *B, double *&B, unsigned int N){
+void dgemm0(const double *A, const double *B, double *&C, unsigned int N){
+	for(unsigned int i = 0; i < N ;i++){
+		for(unsigned int j = 0; j < N ;j++){
+			for(unsigned int k = 0; k < N ;k++){
+				C[i * N + j] += A[ i * N + k ] * B[ k * N + j ];
+			}
+		}
+	}
 
+}
 
+void dgemm1(const double *A, const double *B, double *&C, unsigned int N){
+	for(unsigned int i = 0; i < N ;i++){
+		for(unsigned int j = 0; j < N ;j++){
+			double rC = 0.0f;
+			for(unsigned int k = 0; k < N ;k++){
+				rC += A[ i * N + k ] * B[ k * N + j ];
+			}
+			C[ i * N + j ] = 0;
+		}
+	}
+
+}
+
+void dgemm2(const double *A, const double *B, double *&C, unsigned int N){
+
+	for(unsigned int i = 0; i < N ;i+=2){
+		for(unsigned int j = 0; j < N ;j+=2){
+			double rC0 = 0.0f;
+			double rC1 = 0.0f;
+			double rC2 = 0.0f;
+			double rC3 = 0.0f;
+
+			for(unsigned int k = 0; k < N ;k+=2){
+				rC += A[ i * N + k ] * B[ k * N + j ];
+			}
+			C[ i * N + j ] = 0;
+		}
+	}
 }
 
 int main(int argc, char **argv){
@@ -40,11 +77,16 @@ int main(int argc, char **argv){
 	B = new double[N * N];
 	C = new double[N * N];
 
-	initMatrix(A,B,N);
+	initMatrix(A,B,C,N);
+	Time<secs> t;
 
-	for(int i =0;i<10;i++){
-		std::cout << A[i] <<"x" << B[i] << std::endl;
-	}
+	t.start();
+	dgemm0(A,B,C,N);
+	t.lap("Elapsed time for dgemm0 in secs");
+
+	t.start();
+	dgemm1(A,B,C,N);
+	t.lap("Elapsed time for dgemm1 in secs");
 
 	delete A;
 	delete B;
